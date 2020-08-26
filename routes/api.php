392bb53f9601ad3cd4b2ffc9b18080c0c3326d8b
buddhi-sentura <56859448+buddhi29@users.noreply.github.com>
2020-08-26 12:05:35 +0530

    save paper data

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('quizzes/get_quizzes_of_course_course_set_exam_paper_cat_id/{course_id}/{course_set_id}/{exam_id}/{paper_cat_id}','QuizController@get_quizzes_of_course_course_set_exam_paper_cat_id');
 Route::get('exam/get_exam_of_course_course_set_id/{course_id}/{course_set_id}','examController@get_exam_of_course_course_set_id');
+
+
+Route::get('listening-questions/listening-questions-answers-save','ListeningQuestionsAnswersController@save');
+Route::get('speaking-questions-answers/speaking-questions-answers-save','SpeakingQuestionsAnswersController@save');
+
+
+Route::get('/getPaperUi','ListeningQuestionsAnswersController@getPaperUi');
