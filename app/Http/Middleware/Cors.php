dc2706cab36aebe40a79328513740ebbe49cf6cd
buddhi-sentura <56859448+buddhi29@users.noreply.github.com>
2020-08-26 12:12:48 +0530

    save paper data

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -3,14 +3,28 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use http\Client\Response;
 
 class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access - Control - Allow - Origin', '*')
-            ->header('Access - Control - Allow - Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access - Control - Allow - Headers', 'X - Requested - With, Content - Type, X - Token - Auth, Authorization');
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => '*',
+            'Access-Control-Allow-Headers' => '*'
+        ];
+        if ($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
     }
 }
